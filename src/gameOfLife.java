75142224fb7c3ab32068f45a11b8d19192ee562b
thisisnic <thisisnic@gmail.com>
2016-01-29 18:22:12 +0000

    tidier code with fewer irrelevant classes

--- src/gameOfLife.java
+++ src/gameOfLife.java
@@ -4,8 +4,11 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+
+	/**
+	 * Sleeps the thread for 90 milliseconds
+	 */
 	class aThread extends Thread {
-	
 	    public void run() {
 	    	try {
 				Thread.sleep(90);
@@ -16,20 +19,26 @@ import javax.swing.JPanel;
 	    }
 	}
 
+/**
+ * 
+ */
 class MyCanvas extends JComponent {
 
   public void paint(Graphics g) {
-	 int sideLength = 30;
-	 int numWide = gameOfLife.size;
-	 int numHigh = gameOfLife.size;
+	 int sideLength = 30; // Size of each square 
+	 int numWide = gameOfLife.size; // Width of the grid
+	 int numHigh = gameOfLife.size; // Height of the grid
 	 
-	 int rowCount = 0;
-	 for(int i = 10; i<=numWide*sideLength;i+=sideLength){ //numwide*size wrong?
+	 /**
+	  * Cycle through the array of cells and determine what colour to paint each cell
+	  *  based on the aliveNow attribute.
+	  */
+	 int rowCount = 0; 
+	 for(int i = 10; i<=numWide*sideLength;i+=sideLength){ 
 		 
 		 int colCount = 0;
 		 for(int j = 10;j<=numHigh*sideLength;j+=sideLength){
-			 //System.out.println("r:"+rowCount+" "+colCount);
-			 // If cell[][].alivenow is true, else
+			 
 			 if(gameOfLife.cells[rowCount][colCount].aliveNow==true){
 				 g.setColor(Color.WHITE);
 			 }
@@ -46,77 +55,20 @@ class MyCanvas extends JComponent {
   
 }
 
-
-
  class cell{
-	 
-	 boolean aliveNow = false;;
+	 boolean aliveNow = false;
 	 boolean aliveNext = false; 
-	
 }
 
 public class gameOfLife {
-	static int size = 30;
+	static int size = 30; // Length of grid edge, in squares
 	
-	// between one iteration and the next, the central block turns off
-	public static void drawIt(){
-		 
-		 int numWide = size;
-		 int numHigh = size;
-		 
-		 int rowCount = 0;
-		 /**
-		   _ _ _ _ _ _ _ _ _ _ 
-		  | |X| |X| |X|X| | |X|
-		  | |X| |X| |X|X| | |X|
-		  | |X| |X| |X|X| | |X|
-		  | |X| |X| |X|X| | |X|
-		  | |X| |X| |X|X| | |X|
 
-		  */
-		 String print = " ";
-		 for(int i=0;i<size;i++){
-			 print = print+"_ ";
-		 }
-		 
-		 
-		 print = print+'\n';
-		 String x;
-		 for(int i = 0; i<size;i++){ //numwide*size wrong?
-			 x="|";
-			 int colCount = 0;
-			 
-			 for(int j = 0;j<size;j++){
-				 
-				 // If cell[][].alivenow is true, else
-				 if(gameOfLife.cells[rowCount][colCount].aliveNow==true){
-					 x = (x+"X|");
-					 
-					// System.out.println("l");
-				 }
-				 else{
-					x=(x+" |");
-					
-
-				 }
-				 		
-				 //g.fillRect(i, j, size, size);
-				 colCount ++;
-			 }
-			 print=print+x+'\n';
-			 rowCount++;
-		 }
-		
-		 print = print+" ";
-		 for(int i=0;i<size;i++){
-			 print = print+"‾ ";
-		 }
-		 
-		 System.out.println(print);
-	  }
+	/*
+	 * Create and populate an array of cells on the grid
+	 */
 	
-	
-	static cell[][] cells = new cell[size][size];
+	static cell[][] cells = new cell[size][size]; // array of cells on the grid
 	
 	public static void popCells(){
 		for(int i=0;i<size;i++){
@@ -125,6 +77,9 @@ public class gameOfLife {
 			}
 		}
 		
+		/*
+		 * For demonstration purposes, a number of cells are set to be alive at the start
+		 */
 		cells[1][3].aliveNow=true;
 		cells[2][1].aliveNow=true;
 		cells[2][3].aliveNow=true;
@@ -140,122 +95,94 @@ public class gameOfLife {
 		cells[19][15].aliveNow=true;
 		cells[19][16].aliveNow=true;
 		cells[19][17].aliveNow=true;
+		cells[18][17].aliveNow=true;
 		
 	}
 	
-	// This method is broke. Check neighbours produces the right results tho.
+	/*
+	 * For each cell, calculate where it will be alive or dead on the next iteration.
+	 */
 	public static void calcCells(){
 		for(int i=0;i<size;i++){
 			for(int j=0;j<size;j++){
-				int n = checkNeighbours(i,j);
-				//System.out.println("Before: Alive?"+cells[i][j].aliveNow+"/n?"+n+"/Alive next?"+cells[i][j].aliveNext);
-				//fucks up before here
-				//System.out.println(i+","+j+": "+n);
-				// If the cell is currently alive...
+				int n = checkNeighbours(i,j); // count the number of alive neighbours for this cell
+				
+				// If the current cell is alive...
 				if(cells[i][j].aliveNow==true){
 					
-					// If it has 2 or 3 neighbours it lives
+					// ...and it has 2 or 3 neighbours it lives...
 					if((n==2)||(n==3)){
 						cells[i][j].aliveNext = true;
 					}
-					// Else it dies
+					// ...else it dies.
 					else{
 						cells[i][j].aliveNext = false;
 					}	
 				}
 				
-				// If it's dead currently but has 3 neighbours, it comes to life
+				// If the current cell is dead...
 				else{
+					// ...if it has 3 neighbours, it comes to life...
 					if(n==3){
 						cells[i][j].aliveNext = true;
 					}
+					// ...else it dies.
 					else{
 						cells[i][j].aliveNext = false;
 					}
 				}
-				//System.out.println("After: Alive?"+cells[i][j].aliveNow+"/n?"+n+"/Alive next?"+cells[i][j].aliveNext);
 			}
 		}
-		
-		//Fucks up here? :S
-		
-		// Updates the current status so we can paint this.
-		
-		
 	}
 	
+	
+	/*
+	 * Cycle through all cells and take the value for in aliveNext and assign it to aliveNow.
+	 */
 	public static void updateCells(){
 		for(int i=0;i<size;i++){
 			for(int j=0;j<size;j++){
-				//System.out.println("In loop, before:"+i+","+j+": now="+cells[i][j].aliveNow+"/next="+cells[i][j].aliveNext);
-				cells[i][j].aliveNow = cells[i][j].aliveNext;
-				
-				//System.out.println("In loop, after:"+i+","+j+": now="+cells[i][j].aliveNow+"/next="+cells[i][j].aliveNext);
+				cells[i][j].aliveNow = cells[i][j].aliveNext;		
 			}
 		}
 	}
 	
+	/*
+	 * Given the row and column number of a cell, return the number of neighbours it has.
+	 */
 	public static int checkNeighbours(int midValR, int midValC){
-		int n = 0;
-		//System.out.println("Cell: "+midValR+", "+midValC);
+		
+		int neighbours = 0;
+		
 		for(int i = -1;i<=1; i++){
 			for(int j=-1;j<=1;j++){
-				int thisCellR = midValR+i;
-				int thisCellC = midValC+j;
-				//System.out.println(thisCellR+" "+thisCellC);
-				//System.out.println();
-				// If values are legit between 0 and size, and it's not the orig cell in question
+				int thisCellR = midValR+i; //The row of the cell being examined
+				int thisCellC = midValC+j; //The column of the cell being examined
 				
-				
+				/*
+				 * If the row and column of the current cell are both between 0 and the maximum value (size of the grid),
+				 * 	making sure to exclude the cell itself...
+				 */
 				if(thisCellR>=0&&thisCellR<size&&thisCellC>=0&&thisCellC<size&&(!((i==0)&&(j==0)))){
 					
-					//System.out.println(i+" "+j);
-					//System.out.println(thisCellR+" "+thisCellC+" examined.");
-					// if that cell is alive, increase n
+					// ...then if the current cell is alive, increase the count of neighbours by 1.
 					if(cells[thisCellR][thisCellC].aliveNow==true){
-					//	System.out.println(thisCellR+" "+thisCellC+" counted.");
-						n++;
+					
+						neighbours++;
 					}
 				}
 			}
 		}
-		//System.out.println("Cell ["+midValR+"]["+midValC+"] has " + n + " living neighbours.");
-		//System.out.println("Total: "+n);
-		return n;
+		
+		return neighbours;
 	}
 	
 	
 	
 	
-	public static void main(String[] a) throws InterruptedException {
-		
-		/**
-		popCells();
-		drawIt();
-		
-		for(int i=0;i<5;i++){
-			
-			calcCells();
-			updateCells();
-			
-			SwingUtilities.invokeLater(new Runnable(){
-			public void run(){
-				try {
-					Thread.sleep((500));	
-				}
-				catch (InterruptedException e1){
-					System.err.println(e1);
-				}
-			}
-		});
-
-
-			drawIt();
-			
-		}
-		*/
-		
-		// LEARN HOW TO CODE AND UPDATE GUI STUFF PROPERLY!!!!!
+	public static void main(String[] a) throws InterruptedException {	
+	
+		int totalUpdates = 120; // The number of times to update the grid.
 		
 		popCells();
 		
@@ -266,29 +193,19 @@ public class gameOfLife {
 		calcCells();
 		updateCells();
 		
-		for(int i=0;i<100;i++){
+		for(int i=0;i<totalUpdates;i++){
 			MyCanvas v = new MyCanvas();
-			myPanel.removeAll();
-			myPanel.invalidate();
-			myPanel.add(v);
-			myPanel.revalidate();
-			window.repaint();
+			
 			window.getContentPane().add(v);
 			window.setLocationRelativeTo(null);
 			window.setVisible(true);
 			aThread myThread = new aThread();
 			myThread.start();
 			myThread.join();
-			
-				
+							
 			calcCells();
 			updateCells();
 		}
-		
-		
-			
-		
-		
 		
 	}
 }
